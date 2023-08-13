f4ceda2ef7c7ef420d63d0fdd5dca15ccf1f30b4
Artyom Efimenko <efadko@gmail.com>
2023-08-13 18:55:02 +0300

    refactor: rm flutter project, init dart project

--- android/app/src/main/kotlin/ru/efimenko/dart_ffi/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package ru.efimenko.dart_ffi
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ bin/dart_ffi.dart
@@ -0,0 +1,5 @@
+import 'package:dart_ffi/dart_ffi.dart' as dart_ffi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_ffi.calculate()}!');
+}
